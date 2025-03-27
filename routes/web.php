d6eaa17b7395c6e3c66c0fa785cef0addacc66a8
Feliperios222 <feliperios2003brs@gmail.com>
2025-03-26 19:47:46 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
+use Illuminate\Database\Query\IndexHint;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comuna', [ComunaController::class, 'index']);
+
